b1649fa823db2145e515b503216d82b26dac5241
katriel <katrielaraujo@gmail.com>
2024-01-13 16:45:13 -0300

    mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
+        curso2.setTitulo("curso js");\
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
 
